d145b325aa79e058dde1761e6f15c16ba8ff75c2
SilentViper111 <49122650+SilentViper111@users.noreply.github.com>
2025-09-18 18:02:50 +0200

    Update CustomEmailSender.php

--- src/CustomEmailSender.php
+++ src/CustomEmailSender.php
@@ -2,12 +2,23 @@
 
 namespace Dniccum\CustomEmailSender;
 
+use Laravel\Nova\Nova;
+use Laravel\Nova\Tool;
 use Illuminate\Http\Request;
 use Laravel\Nova\Menu\MenuSection;
-use Laravel\Nova\Tool;
 
 class CustomEmailSender extends Tool
 {
+    /**
+     * Perform any tasks that need to happen when the tool is booted.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::script('custom-email-sender', __DIR__.'/../dist/js/tool.js');
+        Nova::style('custom-email-sender', __DIR__.'/../dist/css/tool.css');
+    }
     /**
      * Build the menu that renders the navigation links for the tool.
      *
